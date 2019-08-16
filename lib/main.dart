4a5694249f62b535abefde2546a0a17f2532fee9
Scott Moore (Scott Moore Software) <scott.moore@scottmooresoftware.com>
2019-08-16 21:57:46 +0100

    Create word pair list

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:english_words/english_words.dart' as prefix0;
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+import 'package:flutter/semantics.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,15 +9,51 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'div-int software',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('div-int software'),
-        ),
-        body: const Center(
-          child: const Text('Hello World\nII', textAlign: TextAlign.center),
-        ),
+      title: 'Startup Name Generator',
+      home: RandomWords(),
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return Divider();
+
+          final index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(prefix0.generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  _buildRow(WordPair wordPair) {
+    return ListTile(
+      title: Text(wordPair.asPascalCase, style: _biggerFont),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // final wordPair = WordPair.random();
+    // return Text(wordPair.asPascalCase);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
       ),
+      body: _buildSuggestions(),
     );
   }
 }
